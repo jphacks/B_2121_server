ba7eebcf950b86c9af280ff82677d40265cb385f
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-10-27 00:25:45 +0900

    Add DB schema draft

---
+++ migrations/20211026224341_Initialize.up.sql
@@ -0,0 +1,90 @@
+CREATE TABLE IF NOT EXISTS `communities`
+(
+    `id`          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `name`        VARCHAR(255) NOT NULL,
+    `description` VARCHAR(511) NOT NULL,
+    `location`    GEOMETRY,
+    `created_at`  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `restaurants`
+(
+    `id`         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `name`       VARCHAR(255) NOT NULL,
+    `location`   GEOMETRY,
+    `url`        VARCHAR(255) NOT NULL,
+    `image_url`  VARCHAR(255),
+    `created_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `communities_restaurants`
+(
+    `id`            BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `community_id`  BIGINT   NOT NULL,
+    `restaurant_id` BIGINT   NOT NULL,
+    `created_at`    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY `fk_community_id_communities_id` (community_id) REFERENCES `communities` (id),
+    FOREIGN KEY `fk_restaurant_id_restaurants_id` (restaurant_id) REFERENCES `restaurants` (id),
+    UNIQUE `u_community_id_restaurant_id` (community_id, restaurant_id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `users`
+(
+    `id`                BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `name`              VARCHAR(255) NOT NULL,
+    `profile_image_url` VARCHAR(511),
+    `created_at`        DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`        DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `affiliation`
+(
+    `id`           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `community_id` BIGINT   NOT NULL,
+    `user_id`      BIGINT   NOT NULL,
+    `created_at`   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY `fk_community_id_communities_id` (community_id) REFERENCES `communities` (id),
+    FOREIGN KEY `fk_user_id_users_id` (user_id) REFERENCES `users` (id),
+    UNIQUE `u_community_id_user_id` (community_id, user_id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `bookmarks`
+(
+    `id`           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `community_id` BIGINT   NOT NULL,
+    `user_id`      BIGINT   NOT NULL,
+    `created_at`   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY `fk_community_id_communities_id` (community_id) REFERENCES `communities` (id),
+    FOREIGN KEY `fk_user_id_users_id` (user_id) REFERENCES `users` (id),
+    UNIQUE `u_community_id_user_id` (community_id, user_id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `comments`
+(
+    `id`            BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `community_id`  BIGINT        NOT NULL,
+    `restaurant_id` BIGINT        NOT NULL,
+    `body`          VARCHAR(1023) NOT NULL DEFAULT '',
+    `created_at`    DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`    DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY `fk_community_id_communities_id` (community_id) REFERENCES `communities` (id),
+    FOREIGN KEY `fk_restaurant_id_restaurants_id` (restaurant_id) REFERENCES `restaurants` (id),
+    UNIQUE `u_community_id_restaurant_id` (community_id, restaurant_id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_bin;
+
