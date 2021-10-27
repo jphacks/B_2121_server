43a9d7476e0a88b9f6181607c584e4322962182a
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-10-28 01:13:06 +0900

    Impl GetCommunityByID

--- db/community.go
+++ db/community.go
@@ -18,11 +18,24 @@ func NewCommunityRepository(db *sqlx.DB) models.CommunityRepository {
 }
 
 func (c *communityRepository) GetCommunityByID(ctx context.Context, id int64) (*models.CommunityDetail, error) {
-	_, err := models_gen.FindCommunity(ctx, c.db, id)
+	community, err := models_gen.FindCommunity(ctx, c.db, id)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to find a community by ID: %w", err)
 	}
 
-	//TODO: impl
-	return &models.CommunityDetail{}, nil
+	numRestaurants, err := community.CommunitiesRestaurants().Count(ctx, c.db)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to count restaurants related to the community: %w", err)
+	}
+
+	userCount, err := community.Affiliations().Count(ctx, c.db)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to count users related to the community: %w", err)
+	}
+
+	return &models.CommunityDetail{
+		Community:     models.Community{Community: *community},
+		NumRestaurant: int(numRestaurants),
+		UserCount:     int(userCount),
+	}, nil
 }
