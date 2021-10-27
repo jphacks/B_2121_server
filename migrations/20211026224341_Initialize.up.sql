5f8f1826b079b03580b26f3a030326df1106d925
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-10-27 20:42:39 +0900

    Replace DATETIME with TIMESTAMP

--- migrations/20211026224341_Initialize.up.sql
+++ migrations/20211026224341_Initialize.up.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS `communities`
     `description` VARCHAR(511) NOT NULL,
     `location`    GEOMETRY,
     `image_file`  VARCHAR(255) NOT NULL COMMENT '画像のファイル名',
-    `created_at`  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `created_at`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_bin;
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS `restaurants`
     `image_url`  VARCHAR(255) COMMENT '画像のURL',
     `source`     VARCHAR(255) NOT NULL COMMENT 'レストラン情報の取得元',
     `source_id`  VARCHAR(255) NOT NULL COMMENT 'レストラン情報の取得元のレストランID',
-    `created_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `created_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_bin;
@@ -29,9 +29,9 @@ CREATE TABLE IF NOT EXISTS `restaurants`
 CREATE TABLE IF NOT EXISTS `communities_restaurants`
 (
     `id`            BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `community_id`  BIGINT   NOT NULL,
-    `restaurant_id` BIGINT   NOT NULL,
-    `created_at`    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `community_id`  BIGINT    NOT NULL,
+    `restaurant_id` BIGINT    NOT NULL,
+    `created_at`    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY `fk_community_id_communities_id` (community_id) REFERENCES `communities` (id)
         ON UPDATE RESTRICT ON DELETE CASCADE,
     FOREIGN KEY `fk_restaurant_id_restaurants_id` (restaurant_id) REFERENCES `restaurants` (id)
@@ -46,8 +46,8 @@ CREATE TABLE IF NOT EXISTS `users`
     `id`                 BIGINT AUTO_INCREMENT PRIMARY KEY,
     `name`               VARCHAR(255) NOT NULL,
     `profile_image_file` VARCHAR(511) COMMENT 'プロフィール画像のファイル名',
-    `created_at`         DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`         DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `created_at`         TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`         TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_bin;
@@ -55,9 +55,9 @@ CREATE TABLE IF NOT EXISTS `users`
 CREATE TABLE IF NOT EXISTS `affiliation`
 (
     `id`           BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `community_id` BIGINT   NOT NULL,
-    `user_id`      BIGINT   NOT NULL,
-    `created_at`   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `community_id` BIGINT    NOT NULL,
+    `user_id`      BIGINT    NOT NULL,
+    `created_at`   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY `fk_community_id_communities_id` (community_id) REFERENCES `communities` (id)
         ON UPDATE RESTRICT ON DELETE CASCADE,
     FOREIGN KEY `fk_user_id_users_id` (user_id) REFERENCES `users` (id)
@@ -70,9 +70,9 @@ CREATE TABLE IF NOT EXISTS `affiliation`
 CREATE TABLE IF NOT EXISTS `bookmarks`
 (
     `id`           BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `community_id` BIGINT   NOT NULL,
-    `user_id`      BIGINT   NOT NULL,
-    `created_at`   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `community_id` BIGINT    NOT NULL,
+    `user_id`      BIGINT    NOT NULL,
+    `created_at`   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY `fk_community_id_communities_id` (community_id) REFERENCES `communities` (id)
         ON UPDATE RESTRICT ON DELETE CASCADE,
     FOREIGN KEY `fk_user_id_users_id` (user_id) REFERENCES `users` (id)
@@ -88,8 +88,8 @@ CREATE TABLE IF NOT EXISTS `comments`
     `community_id`  BIGINT        NOT NULL,
     `restaurant_id` BIGINT        NOT NULL,
     `body`          VARCHAR(1023) NOT NULL DEFAULT '',
-    `created_at`    DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`    DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `created_at`    TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`    TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY `fk_community_id_communities_id` (community_id) REFERENCES `communities` (id)
         ON UPDATE RESTRICT ON DELETE CASCADE,
     FOREIGN KEY `fk_restaurant_id_restaurants_id` (restaurant_id) REFERENCES `restaurants` (id)
