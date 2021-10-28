50e757b2f4fce49f0ec300d125e8de77f42fc39c
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-10-29 00:09:23 +0900

    Impl ListBookmarkByUserId

--- db/bookmark.go
+++ db/bookmark.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/jmoiron/sqlx"
 	"github.com/jphacks/B_2121_server/models"
+	"github.com/jphacks/B_2121_server/models_gen"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"golang.org/x/xerrors"
 )
 
@@ -13,8 +15,20 @@ type bookmarkRepository struct {
 }
 
 func (b *bookmarkRepository) ListBookmarkByUserId(ctx context.Context, userId int64) ([]models.Community, error) {
-	//TODO: impl
-	return []models.Community{}, nil
+	comms, err := models_gen.Communities(
+		qm.InnerJoin("bookmarks ON bookmarks.community_id = communities.id"),
+		qm.Where("user_id = ?", userId),
+	).All(ctx, b.db)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := make([]models.Community, 0, len(comms))
+	for _, comm := range comms {
+		ret = append(ret, models.Community{*comm})
+	}
+
+	return ret, nil
 }
 
 func (b *bookmarkRepository) CreateBookmark(ctx context.Context, userId, communityId int64) error {
