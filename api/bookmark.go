7f30e56503e6fcc487f12548cf43a3089ca28671
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-10-28 23:18:45 +0900

    Impl PostUserIdBookmark

--- api/bookmark.go
+++ api/bookmark.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/jphacks/B_2121_server/openapi"
+	"github.com/jphacks/B_2121_server/session"
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,7 +13,29 @@ func (h handler) GetUserIdBookmark(ctx echo.Context, id openapi.Long) error {
 }
 
 func (h handler) PostUserIdBookmark(ctx echo.Context, id openapi.Long) error {
-	panic("implement me")
+	info := session.GetAuthInfo(ctx)
+	if !info.Authenticated {
+		return echo.ErrUnauthorized
+	}
+	userId := info.UserId
+
+	if int64(id) != userId {
+		return echo.ErrUnauthorized
+	}
+
+	var req openapi.PostUserIdBookmarkJSONRequestBody
+	err := ctx.Bind(&req)
+	if err != nil {
+		ctx.Logger().Errorf("failed to bind request: %v", err)
+		return echo.ErrBadRequest
+	}
+
+	err = h.bookmarkUseCase.CreateBookmark(ctx.Request().Context(), userId, int64(req.CommunityId))
+	if err != nil {
+		return err
+	}
+
+	return ctx.NoContent(http.StatusCreated)
 }
 
 func (h handler) DeleteUserIdBookmarkCommunityId(ctx echo.Context, id openapi.Long, communityId openapi.Long) error {
