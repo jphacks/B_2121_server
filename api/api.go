00312e5af3baf3607b37dac1df097acfed280827
KMConner <KMConner@users.noreply.github.com>
2021-10-28 11:53:48 +0900

    Fix errors

--- api/api.go
+++ api/api.go
@@ -1,10 +1,6 @@
 package api
 
 import (
-	"io/ioutil"
-	"database/sql"
-	"net/http"
-
 	"github.com/jphacks/B_2121_server/openapi"
 	"github.com/jphacks/B_2121_server/usecase"
 	"github.com/labstack/echo/v4"
@@ -20,45 +16,6 @@ func NewHandler(userUseCase usecase.UserUseCase, communityUseCase usecase.Commun
 type handler struct {
 	userUseCase      usecase.UserUseCase
 	communityUseCase usecase.CommunityUseCase
-}
-
-func (h handler) UploadProfileImage(ctx echo.Context) error {
-	info := session.GetAuthInfo(ctx)
-	if !info.Authenticated {
-		return echo.ErrUnauthorized
-	}
-	userId := info.UserId
-	data, err := ioutil.ReadAll(ctx.Request().Body)
-	if err != nil {
-		return err
-	}
-	prof, err := h.userUseCase.UpdateUserProfileImage(userId, data)
-	if err != nil {
-		return err
-	}
-	return ctx.JSON(http.StatusOK, prof)
-func (h handler) NewCommunity(ctx echo.Context) error {
-	panic("implement me")
-}
-
-func (h handler) SearchCommunities(ctx echo.Context, params openapi.SearchCommunitiesParams) error {
-	panic("implement me")
-}
-
-func (h handler) GetCommunityById(ctx echo.Context, id int) error {
-	community, err := h.communityUseCase.GetCommunity(ctx.Request().Context(), int64(id))
-	if err != nil {
-		if xerrors.Is(err, sql.ErrNoRows) {
-			return echo.ErrNotFound
-		}
-		return err
-	}
-
-	return ctx.JSON(http.StatusOK, community.ToOpenApiCommunityDetail())
-}
-
-func (h handler) ListCommunityRestaurants(ctx echo.Context, id int, params openapi.ListCommunityRestaurantsParams) error {
-	panic("implement me")
 }
 
 func (h handler) AddRestaurantToCommunity(ctx echo.Context, id int) error {
