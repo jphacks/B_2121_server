8361e90c4336fbd3a7b6b8e188d71af76b0ceca5
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-10-28 22:59:04 +0900

    Add Bookmark repository

---
+++ models/bookmark.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"context"
+
+	"github.com/jphacks/B_2121_server/models_gen"
+)
+
+type Bookmark struct {
+	models_gen.Bookmark
+}
+
+type BookmarkRepository interface {
+	CreateBookmark(ctx context.Context, userId, communityId int64) (*Bookmark, error)
+}
