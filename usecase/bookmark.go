26b12dd231d12a86d3259e3fc8e58d8c3957d63a
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-10-28 23:07:17 +0900

    Impl CreateBookmark usecase

--- usecase/bookmark.go
+++ usecase/bookmark.go
@@ -1,8 +1,23 @@
 package usecase
 
-func NewBookmarkUseCase() BookmarkUseCase {
-	return BookmarkUseCase{}
+import (
+	"context"
+
+	"github.com/jphacks/B_2121_server/models"
+)
+
+func NewBookmarkUseCase(bookmarkRepo models.BookmarkRepository) BookmarkUseCase {
+	return BookmarkUseCase{bookmarkRepo: bookmarkRepo}
 }
 
 type BookmarkUseCase struct {
+	bookmarkRepo models.BookmarkRepository
+}
+
+func (u *BookmarkUseCase) CreateBookmark(ctx context.Context, userId, communityId int64) error {
+	_, err := u.bookmarkRepo.CreateBookmark(ctx, userId, communityId)
+	if err != nil {
+		return err
+	}
+	return nil
 }
