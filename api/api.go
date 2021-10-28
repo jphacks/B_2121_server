826c76f95a73859af81195c097e49190337f06ae
KMConner <KMConner@users.noreply.github.com>
2021-10-28 15:30:36 +0900

    Move api handlers

--- api/api.go
+++ api/api.go
@@ -18,26 +18,6 @@ type handler struct {
 	communityUseCase usecase.CommunityUseCase
 }
 
-func (h handler) AddRestaurantToCommunity(ctx echo.Context, id int) error {
-	panic("implement me")
-}
-
-func (h handler) RemoveRestaurantFromCommunity(ctx echo.Context, id int64, restaurantId int64) error {
-	panic("implement me")
-}
-
-func (h handler) GetRestaurantComment(ctx echo.Context, id int, restaurantId int) error {
-	panic("implement me")
-}
-
-func (h handler) UpdateRestaurantComment(ctx echo.Context, id int, restaurantId int) error {
-	panic("implement me")
-}
-
-func (h handler) SearchRestaurants(ctx echo.Context, params openapi.SearchRestaurantsParams) error {
-	panic("implement me")
-}
-
 func (h handler) GetUserIdBookmark(ctx echo.Context, id openapi.Long) error {
 	panic("implement me")
 }
---
+++ api/restaurantsHandler.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"github.com/jphacks/B_2121_server/openapi"
+	"github.com/labstack/echo/v4"
+)
+
+func (h handler) AddRestaurantToCommunity(ctx echo.Context, id int) error {
+	panic("implement me")
+}
+
+func (h handler) RemoveRestaurantFromCommunity(ctx echo.Context, id int64, restaurantId int64) error {
+	panic("implement me")
+}
+
+func (h handler) GetRestaurantComment(ctx echo.Context, id int, restaurantId int) error {
+	panic("implement me")
+}
+
+func (h handler) UpdateRestaurantComment(ctx echo.Context, id int, restaurantId int) error {
+	panic("implement me")
+}
+
+func (h handler) SearchRestaurants(ctx echo.Context, params openapi.SearchRestaurantsParams) error {
+	panic("implement me")
+}
