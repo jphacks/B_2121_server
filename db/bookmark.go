bb0b3f57503c8170841015d43f11dd0dcbd4dc87
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-10-28 23:33:18 +0900

    Add ListBookmarkByUserId

--- db/bookmark.go
+++ db/bookmark.go
@@ -2,14 +2,28 @@ package db
 
 import (
 	"context"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/jphacks/B_2121_server/models"
+	"github.com/jphacks/B_2121_server/models_gen"
 	"golang.org/x/xerrors"
 )
 
 type bookmarkRepository struct {
 	db *sqlx.DB
+}
+
+func (b *bookmarkRepository) ListBookmarkByUserId(ctx context.Context, userId int64) (*[]models.Bookmark, error) {
+	//TODO: impl
+	return &[]models.Bookmark{
+		{Bookmark: models_gen.Bookmark{
+			ID:          0,
+			CommunityID: 1,
+			UserID:      2,
+			CreatedAt:   time.Now(),
+		}},
+	}, nil
 }
 
 func (b *bookmarkRepository) CreateBookmark(ctx context.Context, userId, communityId int64) error {
