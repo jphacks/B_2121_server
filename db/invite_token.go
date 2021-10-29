9ee4299ec0ed48bf83a5f0513075ece3ef9a9ef6
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-10-29 16:08:59 +0900

    Implemented Issue token

---
+++ db/invite_token.go
@@ -0,0 +1,52 @@
+package db
+
+import (
+	"context"
+	"crypto/sha256"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/jphacks/B_2121_server/models"
+	"github.com/jphacks/B_2121_server/models_gen"
+	"github.com/lithammer/shortuuid/v3"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+const (
+	inviteTokenExpiresIn = 24 * time.Hour
+)
+
+type inviteTokenRepository struct {
+	db *sqlx.DB
+}
+
+func NewInviteTokenRepository(db *sqlx.DB) models.InviteTokenRepository {
+	return &inviteTokenRepository{db: db}
+}
+
+func (r *inviteTokenRepository) Issue(ctx context.Context, communityId int64) (*models.InviteToken, error) {
+	token := shortuuid.New()
+
+	var it models_gen.InviteToken
+	it.TokenDigest = newDigestString(token)
+	it.CommunityID = communityId
+	it.ExpiresAt = time.Now().Add(inviteTokenExpiresIn)
+	err := it.Insert(ctx, r.db, boil.Whitelist("token_digest", "community_id", "expires_at"))
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.InviteToken{
+		Token:     token,
+		ExpiresIn: inviteTokenExpiresIn,
+	}, nil
+}
+
+func (r *inviteTokenRepository) Verify(ctx context.Context, token string) (string, error) {
+	panic("implement me")
+}
+
+func newDigestString(token string) string {
+	b := sha256.Sum256([]byte(token))
+	return string(b[:])
+}
